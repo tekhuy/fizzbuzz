187e27da5b52af2cac42c19b7ee8076b00539673
tekhuy <huyle20@hotmail.com>
2014-12-11 19:57:45 +0000

    1st fizzbuzz attempt  unfinished

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,23 @@
+def is_divisible_by_three?(number)
+  true
+end
+
+def is_not_divisible_by_three?(number)
+  false
+end
+
+def is_divisible_by_five?(number)
+  true
+end
+
+def is_not_divisible_by_five?(number)
+  false
+end
+
+def is_divisible_by_fifteen?(number)
+  true
+end
+
+def is_not_divisible_by_fifteen?(number)
+  false
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,33 @@
+require './lib/fizzbuzz'
+
+describe 'fizzbuzz' do
+
+  context 'fizzbuzz should know if a number is' do
+
+    it 'is divisible by three' do
+      expect(is_divisible_by_three?(3)).to be true
+    end
+
+    it 'is not divisible by three' do
+      expect(is_not_divisible_by_three?(1)).to be false
+    end
+
+    it 'is divisible by five' do
+      expect(is_divisible_by_five?(5)).to be true
+    end
+
+    it 'is not divisible by five' do
+      expect(is_not_divisible_by_five?(1)).to be false
+    end
+
+    it 'is divisible by fifteen' do 
+      expect(is_divisible_by_fifteen?(15)).to be true
+    end
+
+    it 'is not divisible by fifteen' do
+      expect(is_not_divisible_by_fifteen?(1)).to be false
+    end
+
+  end
+  
+end
