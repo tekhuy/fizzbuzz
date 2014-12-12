2ddcea812f65a90419e714d7caa40741cfc707d9
tekhuy <huyle20@hotmail.com>
2014-12-12 10:20:29 +0000

    playing around

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,23 +1,26 @@
+# def is_divisible_by_three?(number)
+#   is_divisible_by(number, 3)
+# end
+
+# def is_divisible_by_five?(number)
+#   return true if number % 5 == 0
+#   return false if number % 5 != 0
+# end
+
+# def is_divisible_by_fifteen?(number)
+#   return true if number % 15 == 0
+#   return false if number % 15 != 0
+# end
+
+# def is_divisible_by?(number, divisor)
+#   number % divisor == 0
+# end
+
+
 def is_divisible_by_three?(number)
-  true
+  return true if number % 3 == 0
 end
 
 def is_not_divisible_by_three?(number)
-  false
-end
-
-def is_divisible_by_five?(number)
-  true
-end
-
-def is_not_divisible_by_five?(number)
-  false
-end
-
-def is_divisible_by_fifteen?(number)
-  true
-end
-
-def is_not_divisible_by_fifteen?(number)
   false
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,25 +9,25 @@ describe 'fizzbuzz' do
     end
 
     it 'is not divisible by three' do
-      expect(is_not_divisible_by_three?(1)).to be false
+      expect(is_divisible_by_three?(1)).to be false
     end
 
-    it 'is divisible by five' do
-      expect(is_divisible_by_five?(5)).to be true
-    end
+  #   it 'is divisible by five' do
+  #     expect(is_divisible_by_five?(5)).to be true
+  #   end
 
-    it 'is not divisible by five' do
-      expect(is_not_divisible_by_five?(1)).to be false
-    end
+  #   it 'is not divisible by five' do
+  #     expect(is_divisible_by_five?(1)).to be false
+  #   end
 
-    it 'is divisible by fifteen' do 
-      expect(is_divisible_by_fifteen?(15)).to be true
-    end
+  #   it 'is divisible by fifteen' do 
+  #     expect(is_divisible_by_fifteen?(15)).to be true
+  #   end
 
-    it 'is not divisible by fifteen' do
-      expect(is_not_divisible_by_fifteen?(1)).to be false
-    end
+  #   it 'is not divisible by fifteen' do
+  #     expect(is_divisible_by_fifteen?(1)).to be false
+  #   end
 
-  end
+     end
   
 end
